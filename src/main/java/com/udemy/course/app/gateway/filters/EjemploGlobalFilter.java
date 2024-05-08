c77d2d852413e9b768db908799f560117ab4cfc1
Jonathan Ruge <53587500+ruge2109-Git@users.noreply.github.com>
2024-05-07 22:15:29 -0500

    feat: add post filter

--- src/main/java/com/udemy/course/app/gateway/filters/EjemploGlobalFilter.java
+++ src/main/java/com/udemy/course/app/gateway/filters/EjemploGlobalFilter.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
@@ -17,7 +19,9 @@ public class EjemploGlobalFilter implements GlobalFilter {
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         logger.info("Ejecutando filtro pre");
         return chain.filter(exchange).then(Mono.fromRunnable( () -> {
-
+            logger.info("Ejecutando filtro post");
+            exchange.getResponse().getCookies().add("color", ResponseCookie.from("color","rojo").build());
+            exchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
         } ));
     }
 }
